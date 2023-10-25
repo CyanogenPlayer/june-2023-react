e8836646269b643339e84b3895845474873c33c2
Nazarii Ryhus <superkriper45@gmail.com>
2023-10-25 10:03:52 +0300

    lesson 3 part 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, {useRef} from 'react';
 
 const App = () => {
+    const name = useRef();
+    const age = useRef();
+
+    const save = (e) => {
+        e.preventDefault()
+
+        // console.log(e.target.name.value);
+        // console.log(e.target.age.value);
+
+        console.log(name.current.value);
+        console.log(age.current.value);
+
+        e.target.reset();
+    }
+
     return (
         <div>
-
+            <form onSubmit={save}>
+                <input type="text" placeholder={'name'} ref={name}/>
+                <input type="text" placeholder={'age'} ref={age}/>
+                <button>Save</button>
+            </form>
         </div>
     );
 };
